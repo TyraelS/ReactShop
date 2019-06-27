4d6a9d6274be1c617c05b5bf56b7bfd433299ec8
Oleh Shynkarenko <33977283+TyraelS@users.noreply.github.com>
2019-06-28 01:38:14 +0300

    initial commit

---
+++ src/index.jsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import App from './components/App';
+
+ReactDOM.render(<App theme="red" handleClick={() => {}} />, document.getElementById('root'));
